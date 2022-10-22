6d81f5af5e7455ab3d77032fbe6d9debe0e367e8
alda <aldair.alda27@gmail.com>
2022-10-22 12:40:54 -0500

    First functional aproax

--- calculo_poblacion.py
+++ calculo_poblacion.py
@@ -10,7 +10,6 @@ import fiona
 #importamos los puntos de poblacion
 inegi_path='./Localidades_20100_semicolon.csv'
 inegi_data = pd.read_csv(inegi_path,header=0, delimiter = ";", encoding = 'latin-1')
-
 #Importamos los shp de cobertura
 
 coberturas_dir = './Datos/FM/' #Local path, you can change it to mkae it matc your files route. i.e. '/csv/' , '../../Documents/"
@@ -19,31 +18,32 @@ print(coberturas_dir_list)
 
 
 gdf = gpd.GeoDataFrame(
-    inegi_data, geometry=gpd.points_from_xy(inegi_data.LATITUD, inegi_data.LONGITUD))
+    inegi_data, geometry=gpd.points_from_xy(inegi_data.LONGITUD, inegi_data.LATITUD))
 
-# Create Point objects
-p1 = Point(24.952242, 60.1696017)
-p2 = Point(24.976567, 60.1612500)
-p3 = Point(inegi_data.LATITUD[1], inegi_data.LONGITUD[1])
+gdf.set_crs(epsg=4326, inplace=True)
 
-# Create a Polygon
-coords = [(24.950899, 60.169158), (24.953492, 60.169158), (24.953510, 60.170104), (24.950958, 60.169990)]
-poly = Polygon(coords)
+
 cobertura_1 = gpd.read_file(coberturas_dir_list[0])
 
-schema = {
-    'geometry': 'Polygon',
-    'properties': {'id': 'int'},
-}
-
-# Write a new Shapefile
-with fiona.open(coberturas_dir_list[0], 'w', 'ESRI Shapefile', schema) as c:
-    ## If there are multiple geometries, put the "for" loop here
-    c.write({
-        'geometry': mapping(poly),
-        'properties': {'id': 123},
-    })
+print(gdf.head())
+print(gdf.shape)
 print(cobertura_1)
 
+points_within = gpd.sjoin(gdf,cobertura_1, predicate='within')
 
-print(p3.within(cobertura_1))
+print(points_within)
+print(points_within.shape)
+
+
+# coords = [(24.950899, 60.169158), (24.953492, 60.169158), (24.953510, 60.170104), (24.950958, 60.169990)]
+# poly = Polygon(coords)
+# cobertura_1 = gpd.read_file(coberturas_dir_list[0])
+
+# shape = fiona.open(coberturas_dir[0])
+# print (shape.schema)
+# # {'geometry': 'LineString', 'properties': OrderedDict([(u'FID', 'float:11')])}
+# #first feature of the shapefile
+# first = shape.next()
+# print (first) # (GeoJSON format)
+
+# print(p3.within(cobertura_1))
