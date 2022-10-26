a6f67bb1e7049611f56bf51e953079aca06196f1
alda <aldair.alda27@gmail.com>
2022-10-26 00:00:28 -0500

    avance

--- calculo_poblacion.py
+++ calculo_poblacion.py
@@ -1,33 +1,68 @@
 from shapely.geometry import Point, Polygon, mapping
-import csv 
+import argparse
 import glob
 import os
 import pandas as pd
 import geopandas as gpd
+
+
+parser = argparse.ArgumentParser(description='Calcula la extension territorial y poblacion total de un grupo de coberturas .shp dentro del territorio nacional')
+# Required positional argument
+parser.add_argument('--folder', type=str,
+                    help='Nombre de la carpeta que contiene los poligonos .shp',  default='./datos/coberturas/')
+
+# Optional argument
+parser.add_argument('--nombre_destino', type=str,
+                    help='Nombre de los archivos que se generaran: Todos los archivos resultado tendran este nombre como prefijo', required= True)
+
+# Switch
+parser.add_argument('--map', action='store_true',
+                    help='Si se parsea este argumento, se generará un mapa con los resultados')
+
+args = parser.parse_args()
 
 #importamos los puntos de poblacion
 inegi_path='./Localidades_20100_semicolon.csv'
 inegi_data = pd.read_csv(inegi_path,header=0, delimiter = ";", encoding = 'latin-1')
 #Importamos los shp de cobertura
 
-coberturas_dir = './datos/FM/' #Local path, you can change it to mkae it matc your files route. i.e. '/csv/' , '../../Documents/"
+coberturas_dir = args.folder #Local path, you can change it to mkae it matc your files route. i.e. '/csv/' , '../../Documents/"
 coberturas_dir_list = glob.glob(os.path.join(coberturas_dir, '*.shp'))
-print(coberturas_dir_list)
+print("Archivos de cobertura incluidos: ", len(coberturas_dir_list))
 
-gdf = gpd.GeoDataFrame(
+gdf_base = gpd.GeoDataFrame(
     inegi_data, geometry=gpd.points_from_xy(inegi_data.LONGITUD, inegi_data.LATITUD))
 
-gdf.set_crs(epsg=4326, inplace=True)
+gdf_base.set_crs(epsg=4326, inplace=True)
 
-cobertura_1 = gpd.read_file(coberturas_dir_list[0])
+localidades_incluidas = []
 
-print(gdf.head())
-print(gdf.shape)
-print(cobertura_1)
+localidades_incluidas_series = pd.Series
 
-points_within = gpd.sjoin(gdf,cobertura_1, predicate='within')
+print(args.nombre_destino)
 
-print(points_within)
-print(points_within.shape)
+for cobertura in coberturas_dir_list:
+    cobertura_shp= gpd.read_file(cobertura)
+    points_within = gpd.sjoin(gdf_base,cobertura_shp, predicate='within')
+    print("Procesada estacion: ", cobertura)
+    
+    localidades_incluidas_series.concat(points_within['_ID'])
+    
+    # for i in points_within.index:
+    #     localidades_incluidas.append(points_within["_ID"][i])
+    
+localidades_incluidas_series = localidades_incluidas_series.unique()
+# resultado = set(localidades_incluidas)
+print(localidades_incluidas_series)
+print(localidades_incluidas_series.shape)
+# cobertura_1 = gpd.read_file(coberturas_dir_list[0])
 
-points_within.to_file('./results/map01.shp')
+# print(gdf.head())
+# print(gdf.shape)
+# print(cobertura_1)
+
+
+# print(points_within)
+# print(points_within.shape)
+
+# points_within.to_file('./results/map01.shp')
--- data_visualization.py
+++ data_visualization.py
@@ -1,36 +1,45 @@
+from email.mime import base
 from bokeh.plotting import figure, save
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, GeoJSONDataSource
 
 import pandas as pd
 import geopandas as gpd
 
-p = figure(title="Mapa de cobertura")
-print(p)
-
 base_map = r"./datos/datasets/INEGI_2010/estatal.shp"
-population_dots = r"./results/result.shp"
+population_dots = r"./results/map01.shp"
 
 points_gdf = gpd.read_file(population_dots)
-base_map_gdf = gpd.read_file(base_map)
 print(points_gdf.head)
 
-points_to_plot_source = pd.dataframe()
+base_map_gdf = gpd.read_file(base_map)
+base_map_gdf.set_crs(epsg=6362, inplace=True)
+print(base_map_gdf.crs)
 
-psource = ColumnDataSource(points_gdf)
+base_map_gdf_correction = base_map_gdf.to_crs({'init': 'epsg:4326'})
+print(base_map_gdf_correction)
 
+
+points_to_plot_source = points_gdf[['LONGITUD','LATITUD']]
+print(points_to_plot_source)
+
+base_map_source = GeoJSONDataSource(geojson=base_map_gdf_correction.to_json())
+
+
+p = figure(title="Mapa de cobertura",plot_height = 700 ,
+           plot_width = 1000,output_backend="webgl")
+print(p)
+
+
+p.multi_line('xs', 'ys', source=base_map_source, color='gray',
+                   line_color = 'gray', 
+                   line_width = 2)
+
+psource = ColumnDataSource(points_to_plot_source)
 # Initialize our plot figure
-p = figure(title="A map of Localidades dentro de la estacion")
-
 # Add the points to the map from our 'psource' ColumnDataSource -object
-p.circle('LONGITUD', 'LATITUD', source=psource, color='red', size=10)
+p.circle('LONGITUD', 'LATITUD', source=psource, color='red', size=3)
 
-# Output filepath
-outfp = r"./maps/map01.html"
-
-# Save the map
-save(p, outfp)
-
-# outmap_route = r"./maps/map01.html"
-# save(obj=p, filename=outmap_route)
+outmap_route = r"./maps/map02.html"
+save(obj=p, filename=outmap_route, title='Localidades dentro de Cobertura')
 
   
